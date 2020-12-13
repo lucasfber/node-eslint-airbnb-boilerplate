e0bd4e757c5bb8c5e7164ecb2d6d29fff7563d25
Lucas Bertoldo <lucas.fber@gmail.com>
2020-12-12 22:12:50 -0300

    created PORT const

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3000 || process.env.PORT;
 
 app.get('/', (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is running');
 });
